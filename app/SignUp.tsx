ea0ba878f82a730d48f7bae9d140ade550a3cf86
oneboyfromife <oneboyfromife@gmail.com>
2025-01-06 09:28:40 +0100

    Add password input component and update SignUp screen layout

--- app/SignUp.tsx
+++ app/SignUp.tsx
@@ -3,41 +3,48 @@ import React from "react";
 import { AppView } from "@/components/common/AppViews";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import COLORS from "@/constants/colors";
-import { BoldText, MediumText } from "@/components/common/AppText";
+import {
+  BoldText,
+  MediumText,
+  SemiBoldText,
+} from "@/components/common/AppText";
 import { DefaultButton } from "@/components/common/Button";
-import { DefaultInput, EmailInput } from './../components/common/AppInput';
+import {
+  DefaultInput,
+  EmailInput,
+  PasswordInput,
+} from "./../components/common/AppInput";
 
 export default function SignUp() {
   return (
     <AppView>
-      <TouchableOpacity
+      <SemiBoldText
         style={{
-          width: 40,
-          height: 40,
-          borderRadius: 30,
-          backgroundColor: COLORS.grey,
-          alignItems: "center",
-          justifyContent: "center",
-          paddingLeft: 0.5,
+          textAlign: "center",
+          fontSize: 35,
           marginTop: 20,
-          marginBottom: 20,
+          marginBottom: 5,
         }}
       >
-        <Ionicons name="chevron-back-outline" size={24} color="black" />
-      </TouchableOpacity>
-      <BoldText style={{ fontSize: 24 }}>Create an account</BoldText>
+        Welcome!
+      </SemiBoldText>
       <MediumText
-        style={{
-          fontSize: 16,
-          color: "#3D3D3D",
-          marginTop: 5,
-          marginBottom: 20,
-        }}
+        style={{ textAlign: "center", marginBottom: 20, fontSize: 16 }}
       >
-        Create an account today to streamline your payments and manage your
-        finances effortlessly.
+        Sign into your account.
       </MediumText>
-      <EmailInput title='Email address' placeholder="Enter your email..." />
+      <DefaultInput
+        title="Email/ Mobile Number"
+        placeholder="Enter your email..."
+      />
+      <PasswordInput title="Password" />
+      <MediumText style={{ fontSize: 12, textAlign: "right" }}>
+        Forgot Password?{" "}
+      </MediumText>
+      <DefaultButton title="Sign in" style={{ marginBottom: 20 }} />
+      <MediumText style={{ fontSize: 12, textAlign: "center" }}>
+        Don’t have an Account? Sign Up
+      </MediumText>
     </AppView>
   );
 }
--- components/common/AppInput.js
+++ components/common/AppInput.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { MediumText, BoldText, SemiBoldText } from "./AppText";
 import COLORS from "@/constants/colors";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export const DefaultInput = (props) => {
   return (
@@ -64,6 +65,66 @@ export const EmailInput = (props) => {
           placeholderTextColor="#494949"
           onChangeText={(val) => props.onChangeText(val)}
         />
+      </View>
+    </View>
+  );
+};
+
+export const PasswordInput = (props) => {
+  const [password, setPassword] = useState(null);
+  const [isPasswordShown, setIsPasswordShown] = useState(true);
+
+  return (
+    <View>
+      <SemiBoldText
+        style={{
+          fontSize: 13,
+          marginBottom: 5,
+        }}
+      >
+        {props.title}
+      </SemiBoldText>
+      <View
+        style={{
+          width: "100%",
+          backgroundColor: "#ECECEC",
+          borderRadius: 10,
+          borderBottomColor: "#FE7622",
+          borderRadius: 5,
+          borderBottomWidth: 2,
+          alignItems: "center",
+          justifyContent: "center",
+          paddingLeft: 22,
+          ...props.style,
+        }}
+      >
+        <TextInput
+          placeholder="Enter your password"
+          placeholderTextColor="#494949"
+          secureTextEntry={isPasswordShown}
+          value={password}
+          onChangeText={(val) => props.onChangeText(val)}
+          style={{
+            width: "100%",
+            fontSize: 13,
+            fontFamily: "Sen-Regular",
+            paddingVertical: 10,
+          }}
+        />
+
+        <TouchableOpacity
+          onPress={() => setIsPasswordShown(!isPasswordShown)}
+          style={{
+            position: "absolute",
+            right: 12,
+          }}
+        >
+          {isPasswordShown == true ? (
+            <Ionicons name="eye" size={20} color="black" />
+          ) : (
+            <Ionicons name="eye-off" size={20} color="black" />
+          )}
+        </TouchableOpacity>
       </View>
     </View>
   );
